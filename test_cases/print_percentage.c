1f5ec270d815355998fcab520330c190478c0171
Liams-theCreator <mmeellaallii1@gmail.com>
2024-11-19 19:53:07 +0100

    modified

--- test_cases/print_percentage.c
+++ test_cases/print_percentage.c
@@ -18,6 +18,40 @@ int main()
 	len_2 = ft_printf("Current output:     %%%%%%\n");
 	printf("Expected length:    [%d]\n", len_1);
 	printf("Current length:     [%d]\n", len_2);
-
+	
+	printf("------------\n");
+	
+	printf("Expected output:    ");
+	len_1 = printf("%");
+	printf("\n");
+	printf("Current output:     ");
+	len_2 = ft_printf("%");
+	printf("\n");
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	
+	printf("------------\n");
+	
+	printf("Expected output:    ");
+	len_1 = printf("%   ");
+	printf("\n");
+	printf("Current output:     ");
+	len_2 = ft_printf("%   ");
+	printf("\n");
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	
+	printf("------------\n");
+	
+	printf("Expected output:    ");
+	len_1 = printf("test%");
+	printf("\n");
+	printf("Current output:     ");
+	len_2 = ft_printf("test%");
+	printf("\n");
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	
+	printf("------------\n");
 	return (0);
 }
